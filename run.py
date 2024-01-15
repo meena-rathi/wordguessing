45914290ae605b4c8d6a81edddf8d72bc8ee2f07
meena-rathi <>
2024-01-15 20:23:13 +0000

    add list and gernetae random string

--- run.py
+++ run.py
@@ -1,3 +1,36 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random
+
+def random_word():
+    list_words = ['programming', 'teacher', 'happy', 'world']
+    return random.choice(list_words)
+    # word = " ".join(list_words)
+    # print(word)
+
+def guessing_words(word):
+    guessed_word = ["-"] * len(word)
+    print("Select the guessed word") 
+    print(" ".join(guessed_word))
+    return guessed_word
+
+def secret_words(guessed_word):
+    word = ''.join(guessed_word)
+    attempt = 5
+
+    while(attempt > 0):
+        user_input = input("Enter the guessed word: ")
+  
+        if user_input == word:
+            print("correct")
+            break
+        else:
+            print("Try again")
+            attempt -= 1
+
+    if attempt == 0:
+        print("Sorry, you're out of attempts. The correct word was:", word)
+    return guessed_word
+
+
+word_to_guess = random_word()
+guessed_word = guessing_words(word_to_guess)
+guessed_word = secret_words(word_to_guess)
