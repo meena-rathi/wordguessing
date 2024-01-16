e7a14eb2343e7fe584775a7b982de49c7f5d95ba
meena-rathi <>
2024-01-16 08:14:09 +0000

    hint_privide

--- run.py
+++ run.py
@@ -12,9 +12,11 @@ def guessing_words(word):
     print(" ".join(guessed_word))
     return guessed_word
 
-def secret_words(guessed_word):
-    word = ''.join(guessed_word)
+def secret_words(word):
+    guessed_word = guessing_words(word)
+    #word = ''.join(guessed_word)
     attempt = 5
+    hint_word = 2
 
     while(attempt > 0):
         user_input = input("Enter the guessed word: ")
@@ -25,12 +27,22 @@ def secret_words(guessed_word):
         else:
             print("Try again")
             attempt -= 1
-
+        
+        if attempt == hint_word:
+            provide_hint(word, guessed_word)
     if attempt == 0:
         print("Sorry, you're out of attempts. The correct word was:", word)
     return guessed_word
 
+def provide_hint(word, word_guessed):
+    hint_word = random.choice(word)
+    for i in range(len(word)):
+        if word[i] == hint_word:
+            word_guessed[i] = word[i]
+
+    print(f"hint{hint_word}")
+    print(''.join(word_guessed))
 
 word_to_guess = random_word()
-guessed_word = guessing_words(word_to_guess)
+#guessed_word = guessing_words(word_to_guess)
 guessed_word = secret_words(word_to_guess)
